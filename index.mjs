7b7992b9afcb759ba286b09f3fc68322a34bf9ff
Jose <jxrieradufflar@hotmail.com>
2023-10-14 01:07:14 -0400

    added the get for the main page

--- index.mjs
+++ index.mjs
@@ -9,6 +9,11 @@ app.use(express.json());
 //router
 app.use('/api/products', router);
 
+//get for the main page
+app.get('/', (req, res) => {
+    res.send({ message: 'Welcome to DressStore application' });
+});
+
 
 //Listen port
 app.listen(3000, () => console.log('Server started on port 3000'));
